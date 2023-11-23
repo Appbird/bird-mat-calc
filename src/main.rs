dcb49f9a7f8d8916fe6a9bf8587bf56628acf5ce
appbird <project.appbird@gmail.com>
2023-11-23 18:02:02 +0900

    read matrix with empty-line end

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,15 @@ fn main() {
     let mut matrix = Vec::<Vec::<Fraction>>::new();
     loop {
         let mut input = String::new();
-        let readed_byte = std::io::stdin().read_line(&mut input).expect("Failed to read line.");
-        if readed_byte == 0 { break; } // reached to EOF
+        std::io::stdin().read_line(&mut input).expect("Failed to read line.");
+        if input.trim().len() == 0 { break; }
 
         let number_in_strings:Vec<Fraction> =
             input.split_whitespace()
             .map(|x| Fraction::from_str(x).expect("Failed to parse as number"))
             .collect();
         matrix.push(number_in_strings);
+
     }
     for row in matrix {
         for cell in row {
