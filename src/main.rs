ba886eb4de0368ef46fbb6b3538f8be6e6697234
appbird <project.appbird@gmail.com>
2023-11-23 23:14:14 +0900

    arrange user interface

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,57 @@ use clap::{Parser, Subcommand};
 
 #[derive(Subcommand)]
 enum Commands {
-    /// 二つの行列の積をとる。
-    /// 空白区切りで入力を行い、改行を行えば次の列へ入力される。
-    /// 空行を含めば、次の行列へ入力が移る。
+    /// calculate product of two given matrix through stdin.
     Mul,
-    /// 行列に対して掃き出し法を行う。
+    /// Sweeping one given matrix through stdin.
     Sweep,
+    /// inverse one given matrix through stdin.
+    Inverse,
+
 }
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct CLI {
     #[command(subcommand)]
+    /// 空白区切りで入力を行い、改行を行えば次の列へ入力される。
+    /// 空行を含めば、次の行列へ入力が移る。
     command: Commands
+}
+fn eprint_guide() {
+    eprintln!("To finish entering one matrix, enter empty line.");
 }
 
 fn mul() {
+    eprintln!("Enter A >>");
     let a = Matrix::read_matrix();
+    
+    eprintln!("Enter B (its row should be {}.) >>", a.col());
     let b = Matrix::read_matrix();
+
+    println!("{}", a.mul(&b));
+}
+fn sweep() {
+    eprintln!("Enter A >>");
+    let a = Matrix::read_matrix();
+
+    println!("{}", a.sweeped());
+}
+fn inverse() {
+    eprintln!("inverse A. Finish entering a matrix by enter empty line.");
+    eprintln!("Enter A >>");
+    let a = Matrix::read_matrix();
+
+    println!("{}", a.inversed());
 }
 
 fn main() {
     let cli = CLI::parse();
-    let b = Matrix::read_matrix();
+    eprint_guide();
     match  &cli.command {
-        Commands::Mul => ,
-        Commands::Sweep =>
+        Commands::Mul => mul(),
+        Commands::Sweep => sweep(),
+        Commands::Inverse => inverse(),
     }
     
 }
