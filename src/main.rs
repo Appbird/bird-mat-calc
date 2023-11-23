e95ed121209d6be7a7ee3b0329d9f0a814eedb3e
appbird <project.appbird@gmail.com>
2023-11-23 17:57:20 +0900

    read matrix

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::str::FromStr;
 
 use fraction::Fraction;
 fn main() {
-    let mut input = String::new();
-    for _i in 0..1 {
-        std::io::stdin().read_line(&mut input).expect("Failed to read line.");
+    let mut matrix = Vec::<Vec::<Fraction>>::new();
+    loop {
+        let mut input = String::new();
+        let readed_byte = std::io::stdin().read_line(&mut input).expect("Failed to read line.");
+        if readed_byte == 0 { break; } // reached to EOF
+
         let number_in_strings:Vec<Fraction> =
             input.split_whitespace()
             .map(|x| Fraction::from_str(x).expect("Failed to parse as number"))
             .collect();
-        for n in number_in_strings{
-            print!("{} ", n);
+        matrix.push(number_in_strings);
+    }
+    for row in matrix {
+        for cell in row {
+            print!("{} ", cell);   
         }
         println!();
     }
